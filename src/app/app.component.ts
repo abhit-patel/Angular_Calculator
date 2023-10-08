7606245020a52b998e7a78e108dbc1fc235e706c
2anjali8 <Abhithinsu2121@gmail.con>
2023-10-08 10:23:09 +0530

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular_calculator';
+  toShow = '0';
+  currentValue = ''
+
+  addToInput(value:string){
+    // if(this.currentValue == '0'){
+    //   this.currentValue = value
+    // }
+    // else{
+      this.currentValue = this.currentValue + value;
+    // }
+    this.toShow = this.currentValue;
+
+  }
+
+  Equal(){
+    try{
+      this.toShow = eval(this.currentValue);
+    }
+    catch(error){
+      this.toShow = 'Error' ;
+    }
+    this.currentValue = this.toShow.toString();
+  }
+
+  Clear(){
+    this.toShow = '0';
+    this.currentValue = '';
+  }
+
+  Back(){
+    if (this.currentValue.length > 0) {
+      this.currentValue = this.currentValue.slice(0, -1);
+    }
+
+    if(this.currentValue == ''){
+      this.toShow = '0'}
+    else {
+      this.toShow = this.currentValue;
+    }
+  }
+
+  CalcValue(value:string){
+    if(value.charAt(0) == '0'){
+      value = value.slice(1,)
+    }
+    if(value.length > 0){
+      this.toShow = eval(value)
+      this.currentValue = this.toShow;
+    }
+  }
 }
